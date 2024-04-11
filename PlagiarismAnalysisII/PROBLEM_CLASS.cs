7020ce039e5d377eccf0606c7e0b3e5b48788b49
Ahmed Magdy <ahmedmagdy373@gmail.com>
2024-04-11 21:15:33 +0200

    initial commit

--- PlagiarismAnalysisII/PROBLEM_CLASS.cs
+++ PlagiarismAnalysisII/PROBLEM_CLASS.cs
@@ -8,7 +8,84 @@ namespace Problem
     {
         public static void RequiredFunction(Tuple<string, string, float>[] edges, ref float maxAvgScore, ref List<string> IDs)
         {
-            /* 
+            Array.Sort(edges, (x, y) => y.Item3.CompareTo(x.Item3));
+            float maxSum = 0f;
+            int count = 0;
+
+            float maxScore = 0f;
+
+            Tuple<string, string, float> currentEdge = edges[0];
+            Dictionary<float, List<Tuple<string, string, float>>> neighbors =
+                new Dictionary<float, List<Tuple<string, string, float>>>();
+
+            List<Tuple<string, string, float>> edgesList =
+                new List<Tuple<string, string, float>>();
+            HashSet<string> visited = new HashSet<string>();
+            edgesList.Add(currentEdge);
+            maxSum += currentEdge.Item3;
+            count = 1;
+            visited.Add(currentEdge.Item1);
+            visited.Add(currentEdge.Item2);
+         
+            for (int i = 1; i < edges.Length; i++)
+            {
+                Console.WriteLine(edges[i]);
+                if (visited.Contains(edges[i].Item1)||visited.Contains(edges[i].Item2))
+                {
+                    maxSum += edges[i].Item3;
+                    currentEdge = edges[i];
+                    count++;
+                    edgesList.Add(edges[i]);
+                }
+                else
+                {
+                    maxScore = maxSum / count;
+                    List<Tuple<string, string, float>> newList = new List<Tuple<string, string, float>>();
+
+                    foreach (var edge in edgesList)
+                    {
+                        newList.Add(edge);
+                    }
+
+                    neighbors[maxScore]=newList;
+                    Console.WriteLine(" sum --> " + maxSum + " count --> " + count + " score --> " + maxScore);
+                    maxSum = 0;
+                    maxScore = 0;
+                    count = 1;
+                    edgesList = new List<Tuple<string, string, float>>();
+                    visited.Add(edges[i].Item1);
+                    visited.Add(edges[i].Item2);
+                    maxSum += currentEdge.Item3;
+                    edgesList.Add(currentEdge);
+                }
+                currentEdge = edges[i];
+            }
+            if (count <= 2)
+            {
+                maxScore=maxSum / count;
+                neighbors[maxScore] = edgesList;
+            }
+          
+            KeyValuePair<float, List<Tuple<string, string, float>>> maxKeyValuePair =
+            neighbors.OrderByDescending(kv => kv.Key)
+                     .First();
+
+            List<string> idsList = new List<string>();
+            foreach (var edge in maxKeyValuePair.Value)
+            {
+               
+             
+                    idsList.Add(edge.Item1);
+              
+                    idsList.Add(edge.Item2);
+
+            }
+            IDs= idsList.OrderBy(id => id).ToList();
+            IDs = IDs.Distinct().ToList();
+            maxAvgScore = maxKeyValuePair.Key;
+
+
+            /*
              Dictionary<string, List<Tuple<string, float>>> neighbors = new Dictionary<string, List<Tuple<string, float>>>();
              HashSet<string> allNodes = new HashSet<string>();
 
@@ -87,7 +164,6 @@ namespace Problem
 
 
              */
-            
         }
 
     }
